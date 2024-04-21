b2ee363ecac0b5da2ec94adec8b7e128639a922c
Naymul Islam <naymul504@gmail.com>
2024-04-21 18:14:06 +0600

    updated data_clearning
    Signed-off-by: Naymul Islam <naymul504@gmail.com>

---
+++ data_cleaning.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractclassmethod
+from typing import Union
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class DataWorks(ABC):
+    '''Abstract class defining the strategy for handeling the data'''
+    
+    @abstractclassmethod
+    def handle_data(self, data: pd.DataFrame):
+        pass
+
+
+class DataCleaning(DataWorks):
+    def handle_data(self, data: pd.DataFrame):
+        data.drop([
+            'order_approved_at',
+            'order_delivered_carrier_date',
+            'order_delivered_customer_date',
+            'order_estimated_delivery_date',
+            'order_purchase_timestamp'
+        ], axis=1)
+        
+        data['price'].fillna(data['price'].mean(), inplace=True)
+        data['product_weight_g'].fillna(data['product_weight_g'].median, inplace=True)
+        data['product_length_cm'].fillna(data['product_length_cm'].median, inplace=True)
+        data['product_height_cm'].fillna(data['product_height_cm'].median, inplace=True)
+        data['product_width_cm'].fillna(data['product_width_cm'].median, inplace=True)
+        data['review_score'].fillna(data['review_score'].mean(), inplace=True)
+        data['review_comment_message'].fillna("No review", inplace=True)
+
+        data = data.select_dtypes(include=[np.number])
+        columns_to_drop = ['customer_zip_code_prefix', 'order_item_id']
+        data.drop(columns_to_drop, axis=1)
