2ae625a15cee82b937f0fe6d5733c8f254e4b204
Naymul Islam <naymul504@gmail.com>
2024-04-25 23:16:10 +0600

    add model development
    Signed-off-by: Naymul Islam <naymul504@gmail.com>

--- model_development.py
+++ model_development.py
@@ -8,7 +8,7 @@ import pandas as pd
 import xgboost as xgb
 from lightgbm import LGBMRegressor
 
-
+# TODO Create the base of the Model to use it making models using it in the Models(Linear,XGBoost etc.)
 class Model(ABC):
     @abstractmethod
     def train(self, X_train, y_train):
@@ -19,7 +19,7 @@ class Model(ABC):
         pass
 
 
-
+# TODO Create a RandomForest Model 
 class RandomForestModel(Model):
     
     def train(self, X_train, y_train, **kwargs):
@@ -34,6 +34,7 @@ class RandomForestModel(Model):
         reg = self.train(X_train, y_train, n_estimators= n_estimators, max_depth=max_depth, min_samples_split=min_sample_split)
         return reg.score(X_test, y_test)
 
+# TODO Create a LightGBMMOdel like the before one
 class LightGBMModel(Model):
     
     def train(self, X_train, y_train, **kwargs):
@@ -49,8 +50,47 @@ class LightGBMModel(Model):
         return reg.score(X_test, y_test)
 
 # TODO: Create a XGBoost model like 
+
+class XGBoost(Model):
+    def train(self, X_train, y_train, **kwargs):
+        reg = XGBoost(**kwargs)
+        reg.fit(X_train, y_train)
+        return reg
+    
+    def optimize(self, trial, X_train, y_train, X_test, y_test):
+        n_estimator = trial.suggest_int('n_estimator', 1, 200)
+        max_depth = trial.suggest_int('max_depth', 1, 20)
+        learning_rate = trial.suggest_int('learning_rate', 0.01, 0.99)
+        reg = self.train(X_train, y_train, n_estimator=n_estimator, max_depth=max_depth, learning_rate=learning_rate)
+        return reg.score(X_test, y_test)
+
+
 # TODO: Create a Linear RgressionModel
+
+class LinearRegression(Model):
+    def train(self, X_train, y_train, **kwargs):
+        reg = LinearRegression(**kwargs)
+        reg.fit(X_train, y_train)
+        return reg
+    
+    def optimize(self, X_train, y_train, X_test, y_test):
+        reg = self.train(X_train, y_train)
+        return reg.score(X_test, y_test)
+
+
 # TODO: Create a HyperParameterTuner  
 
+class HyperParameterTuner:
+    
+    def __init__(self, model, X_train, y_train, X_test, y_test):
+        self.model = model
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
 
-
+    
+    def optimize(self, n_trials=100):
+        study = optuna.create_study(direction='maximize')
+        study.optimize(lambda trial: self.model.optimize(trial, self.X_train, self.y_train, self.X_test, self.y_test), n_trials=n_trials)
+        return study.best_trial.params
