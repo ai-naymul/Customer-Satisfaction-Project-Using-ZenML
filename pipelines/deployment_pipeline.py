021a540f501ec88bc5348de9a64f26838cc6912c
Naymul Islam <naymul504@gmail.com>
2024-05-06 12:17:30 +0600

    add deployment pipelines
    Signed-off-by: Naymul Islam <naymul504@gmail.com>

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -1,4 +1,4 @@
-# TODO Imports all liabry
+# Imports all liabry
 
 # imports = json, os, numpy, pandas
 import json
@@ -29,20 +29,127 @@ from .utils import get_data_for_test
 
 docker_setting = DockerSettings(required_integrations=[MLFLOW])
 
-# TODO define the requirements
+# define the requirements
 
-# TODO dynamic importer step
+requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
 
-# TODO Deployment Trigger cofig class
+# dynamic importer step
 
-# TODO deployment trigger step method
 
-# TODO MLFLOw oyment trigger step param class
+@step(enable_cache=True)
+def dynamic_importer():
+    data = get_data_for_test()
+    return data
 
-# TODO prediction service loader step method
+# Deployment Trigger cofig class
 
-# TODO prediction step method
+class DeploymentTriggerConfig(BaseParameters):
+    min_accuracy: float = 0.9
 
-# TODO continious deployment pipeline with docker serttings
+# deployment trigger step method
+@step
+def deployment_trigger(accuracy: float, config: DeploymentTriggerConfig):
+    return accuracy > config.min_accuracy
 
-# TODO inference pipeline with the docker settings
+
+
+# MLFLOw oyment trigger step param class
+class MLFlowDeploymentLoaderStepParameter(BaseParameters):
+    pipelines_name : str
+    step_name: str
+    running: bool = True
+
+
+# prediction service loader step method
+@step(enable_cache=True)
+def prediction_service_loader(pipelines_name: str,
+                              pipeline_step_name: str,
+                              running: bool = True,
+                              model_name : str = 'model',) -> MLFlowDeploymentService:
+    existing_services = MLFlowModelDeployer.get_active_model_deployer(
+        pipelines_name=pipelines_name,
+        pipeline_step_name=pipeline_step_name,
+        model_name=model_name,
+        running=running
+        )
+    
+    if not existing_services:
+        raise RuntimeError(
+            f"No mlflow deployment service deployed by the"
+            f'{pipeline_step_name} step in the {pipelines_name}'
+            f'pipeline for the "{model_name}" model is currently running'
+        )
+    
+    print(existing_services)
+    print(type(existing_services))
+    return existing_services[0]
+
+
+# prediction step method
+
+@step
+def predictor(
+    service: MLFlowDeploymentService,
+    data : np.ndarray,
+) -> np.ndarray:
+    service.start(timeout=10)
+    data = json.load(data)
+    data.pop('columns')
+    data.pop('index')
+    columns_for_df=[
+        'payment_sequential',
+        'payment_installments',
+        'payment_value',
+        'price',
+        'freight_value',
+        'product_name_lenght',
+        'product_description_lenght',
+        'product_photos_qty',
+        'product_weight_g',
+        'product_length_cm',
+        'product_height_cm',
+        'product_width_cm',
+    ]
+    df = pd.DataFrame(data['data'], columns=columns_for_df)
+    json_list = json.load(json.dumps(list(df.T.to_dict().values())))
+    data = np.array(json_list)
+    prediction = service.predict(data)
+    return prediction
+
+
+# continious deployment pipeline with docker serttings
+
+
+
+@pipeline(enable_cache=True, settings={'docker': docker_setting})
+def continous_deployment_pipeline(
+    min_accuracy: float = 0.9,
+    worker: int = 1,
+    timeout: int = DEFAULT_SERVICE_START_STOP_TIMEOUT,
+):
+    df = ingest_data()
+    X_train, X_test, y_train, y_test = clean_data(df)
+    model = train_model(X_train, X_test, y_train,y_test)
+    mse, rmse = evaluate(model, X_test,y_test)
+    deployment_decision = deployment_trigger(accuracy = mse)
+    mlflow_model_deployer_step(
+        model=model,
+        deployment_decision=deployment_decision,
+        worker=worker,
+        timeout=timeout,
+    )
+
+
+# inference pipeline with the docker settings
+@pipeline(enable_cache=True, settings={'docker': docker_setting})
+def inference_pipeline(
+        pipeline_name: str,
+        pipeline_step_name: str,
+):
+    batch_data = dynamic_importer()
+    model_deployment_service = prediction_service_loader(
+        pipeline_name=pipeline_name,
+        pipeline_step_name=pipeline_step_name,
+        running = False
+    )
+    predictor(service = model_deployment_service, data=batch_data)
